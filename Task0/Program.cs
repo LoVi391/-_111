de9601401aea881706c4e17c86c8eb55fae07cfb
unknown <Arknes fool>
2022-05-05 15:49:24 +0300

    1

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,12 +1,26 @@
-﻿///Напишите программу, которая на вход 
-//принимает число и выдаёт его квадрат (число 
-//умноженное на само себя).
+﻿///2. Напишите программу, которая будет выдавать 
+///название дня недели по заданному номеру.
+//3 -> Среда 
+///5 -> Пятница
 
-Console.Write("Введите целое число -> ");
-int num = int.Parse(Console.ReadLine());
-int numResult = num * num;
-Console.Write("Квадрат числа равен -> ");
-Console.WriteLine(numResult);
+Console.Write("Введите целое число a");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите целое число b");
+int num2 = int.Parse(Console.ReadLine());
+///Console.Write("Квадрат числа равен -");
+int numResult = num1 * num1;
+if (numResult == num2)
+{
+Console.Write("Yes");
+}
+else
+{
+Console.Write("No");
+}
+
+
+
+
 Console.ReadKey();
 
 
